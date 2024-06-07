96de3fe72806dd5462c22f7ef53128bd20dd08e2
Maria <malufmatos22@gmail.com>
2024-06-06 23:31:54 -0300

    aula 2

---
+++ aula 2/comentarios.js
@@ -0,0 +1,14 @@
+// Escrever um comentário 
+console.log('Olá mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+//Comentário
+
+/* 
+Um bloco 
+de código
+*/
+
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
