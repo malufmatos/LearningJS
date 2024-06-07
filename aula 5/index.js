bbe9c4f01d29e6dbfbae8fa476b6851296c8465d
Maria <malufmatos22@gmail.com>
2024-06-07 01:05:54 -0300

    aula 5

---
+++ aula 5/index.js
@@ -0,0 +1,14 @@
+/*
+Não podemos criar constantes com palavras reservadas
+Constantes precisam ter nomes significativos
+Não pode começar o nome de uma constante com um número
+Não podem conter espaços ou traços
+Utilizamos camelCase
+Case-sensitive
+Não podemos modificar o valor de uma constante
+Toda constante precisa de uma declaração que terá que ser previamente atribuída
+NÃO UTILIZE VAR, UTILIZE CONST.
+*/
+
+const nome ='João';
+console.log(nome);
