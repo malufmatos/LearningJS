150bbe4f35a2523bd8426fa4181d969fb3688567
Maria <malufmatos22@gmail.com>
2024-06-10 20:44:54 -0300

    aula 11

---
+++ aula 11/index.js
@@ -0,0 +1,14 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+[varA, varB, varC] = [varB, varC, varA]
+
+/*
+const temp = varA;
+varA = varB;
+varB = varC;
+varC = temp;
+*/
+
+console.log(varA, varB, varC);
