91755b91f8115ed4c5da864cbb67eecdcbb826b6
maria luiza <malufmatos22@gmail.com>
2024-06-18 20:56:47 -0300

    aula 14

---
+++ aula 14/index.js
@@ -0,0 +1,11 @@
+let num1 = 10.57981;
+let num2 = 2.5;
+
+//console.log(num1.toString() + num2);
+// num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));
+//console.log(Number.isNaN(temp));
+//let temp = num1 * '5';
+//console.log(temp);
