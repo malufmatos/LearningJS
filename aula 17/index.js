0f4bbc17788904a56528cf9a4d0b8b9cc01998bf
maria luiza <malufmatos22@gmail.com>
2024-06-25 20:05:13 -0300

    aula 17 e 18

--- aula 17/index.js
+++ aula 17/index.js
@@ -1,5 +1,36 @@
 //               0       1        2   
 const alunos = ['Luiz', 'Maria', 'João'];
-console.log(alunos);
-console.log(alunos[0]);
-console.log(alunos[2])
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(-2, -1));
+
+//console.log(alunos[50]);
+
+// delete alunos [1];
+// console.log(alunos[1]);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); //Adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); //Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; //Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; //Alterando
+// alunos[3] = 'Luiza';
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2])
