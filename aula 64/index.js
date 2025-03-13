bbc5c2f55a36d26d13e5ae6066d339fc0f1d9fd2
maria <malufmatos22@gmail.com>
2025-03-13 11:29:37 -0300

    aula 64

---
+++ aula 64/index.js
@@ -0,0 +1,11 @@
+//              -5       -4       -3         -2         -1
+//               0        1        2          3          4
+const nomes = ["Maria", "João", "Eduardo", "Gabriel", "Júlia"];
+
+//Unshift
+nomes.splice(2, 0, "Maria", "Luiza");
+console.log(nomes);
+
+//nomes.splice(índice atual, delete, elem1, elem2, elem3);
+//pop
+//const removidos = nomes.splice(-1, 1);
