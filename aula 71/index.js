025269b8448172906533323c2d8b3b78751edc64
maria luiza <malufmatos22@gmail.com>
2025-04-08 11:33:46 -0300

    aula 71

---
+++ aula 71/index.js
@@ -0,0 +1,18 @@
+/*
+Object.values
+Object.entries
+Object.assign(des,any)
+Object.getOwnPropertyDescriptor(o, 'prop)
+... (spread)
+
+// Já vimos
+Object.keys
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
+const produto = { nome: "Produto", preco: 1.8, material: "porcelana" };
+
+for (let valor of Object.entries(produto)) {
+  console.log(valor[0], valor[1]);
+}
